edaa1266021acee94a882c35ea6b7ffa1c9c5d57
kishan-2029 <kishan.prajapati@searce.com>
2023-08-02 17:02:29 +0530

    prisma module added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable()
+@Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     return 'I am signup';
   }
