51393bf067ef9b2532b6c679674693f545208362
kishan-2029 <kishan.prajapati@searce.com>
2023-08-03 18:12:38 +0530

    validationPipe is implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+
   signup() {
-    return 'I am signup';
+    return { msg: 'I am signup' };
   }
 
   signin() {
